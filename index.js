17a022b2255939bf9c963dc3ec61f2dfa33d94ca
Arzena Akter <arzenaakter@gmail.com>
2023-03-15 23:16:51 +0600

    fix undefined question & submit button

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ const displayQuiz = (data) => {
 };
 
 // EventListener for quiz submit button
-document.querySelector("#submit").addEventlistener("click", () =>{
+document.querySelector("#submit").addEventListener("click",  ()=> {
   if (answers.length < 6) {
     return;
   }
