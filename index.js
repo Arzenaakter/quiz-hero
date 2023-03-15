50ff86abb5a0d3d8202fdc8663793d7f5ce2c99f
Arzena Akter <arzenaakter@gmail.com>
2023-03-15 22:01:24 +0600

    fix start button

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ startQuiz.addEventListener("click", () => {
 
   let x = setInterval(() => {
     if (counterNum < 0) {
-      coutDown.classList.remove("flex");
-      coutDown.classList.add("hidden");
+      countDown.classList.remove("flex");
+      countDown.classList.add("hidden");
       counterNum = 3;
       count = 0;
       timer = null;
@@ -45,9 +45,11 @@ startQuiz.addEventListener("click", () => {
 
 // All quiz data fetched from json
 const loadQuiz = async () => {
-  const res = await fetch("./data/quiz.json");
+  const res = await fetch("data/quiz.json");
   const data = await res.json;
+  console.log(data)
   quizData = data;
+
   displayQuiz(data);
 };
 
@@ -74,7 +76,7 @@ const displayQuiz = (data) => {
 };
 
 // EventListener for quiz submit button
-document.querySelector("#submit").addEventlistener("click", () => {
+document.querySelector("#submit").addEventlistener("click", () =>{
   if (answers.length < 6) {
     return;
   }
